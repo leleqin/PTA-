3160a68211052d5e4fa7bd72d338f4ac8b11c442
leleqin <602568224@qq.com>
2019-03-29 20:35:43 +0800

    规范readme

--- src/practice2/Main.java
+++ src/practice2/Main.java
@@ -7,18 +7,21 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		int a1 = n/100;   //百位
-		int a2 = n%100/10;   //十位
-		int a3 = n%10;    //个位
-		while(a1>0){
+		int a1 = n/100;   //鐧句綅
+		System.out.println(a1);
+		int a2 = n%100/10;   //鍗佷綅
+		System.out.println(a2);
+		int a3 = n%10;    //涓綅
+		System.out.println(a3);
+		while(a1 > 0){
 			System.out.print("B");
 			a1--;
 		}
-		while(a2>0){
+		while(a2 > 0){
 			System.out.print("S");
 			a2--;
 		}
-		for(int i=1 ;i<=a3 ;i++){
+		for(int i = 1 ;i <= a3;i++){
 			System.out.print(i);
 		}
 		System.out.println();
